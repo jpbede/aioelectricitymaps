a1d2c01c7a8752e1eedf307e49254de83a0fe700
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2024-01-02 21:55:51 +0100

    chore: add decorator typing (#57)

--- aioelectricitymaps/decorators.py
+++ aioelectricitymaps/decorators.py
@@ -1,12 +1,21 @@
 """Decorators for aioelectricitymaps."""
+from __future__ import annotations
+
+from collections.abc import Callable, Coroutine
+from typing import Any, ParamSpec, TypeVar
 
 from .exceptions import SwitchedToLegacyAPI
 
+_R = TypeVar("_R")
+_P = ParamSpec("_P")
 
-def retry_legacy(func):
+
+def retry_legacy(
+    func: Callable[_P, Coroutine[Any, Any, _R]]
+) -> Callable[_P, Coroutine[Any, Any, _R]]:
     """Decorator to retry a function with the legacy API if SwitchedToLegacyAPI is raised."""
 
-    async def inner(*args, **kwargs):
+    async def inner(*args: _P.args, **kwargs: _P.kwargs) -> _R:
         try:
             result = await func(*args, **kwargs)
         except SwitchedToLegacyAPI:
