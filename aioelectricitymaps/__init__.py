e67364c3d85ab3d1a2982c7751e0225efd9fbe9a
Jan-Philipp Benecke <jan-philipp@bnck.me>
2024-01-24 17:31:10 +0100

    Migrate dataclasses-json to mashumaro

--- aioelectricitymaps/__init__.py
+++ aioelectricitymaps/__init__.py
@@ -1,4 +1,13 @@
 """ElectricityMaps wrapper."""
 from .electricitymaps import ElectricityMaps
+from .exceptions import ElectricityMapsDecodeError, ElectricityMapsError, InvalidToken
+from .models import CarbonIntensityResponse, Zone
 
-__all__ = ["ElectricityMaps"]
+__all__ = [
+    "ElectricityMaps",
+    "ElectricityMapsDecodeError",
+    "ElectricityMapsError",
+    "InvalidToken",
+    "CarbonIntensityResponse",
+    "Zone",
+]
